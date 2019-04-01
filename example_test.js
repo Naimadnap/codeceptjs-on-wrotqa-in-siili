d4e5ae6a00da5587c88206ccb8a2b3aba679f18c
Damian Zygmuntowicz <damian.zygmuntowicz@siili.com>
2019-04-01 15:16:27 +0200

    Add full test

--- example_test.js
+++ example_test.js
@@ -1,8 +1,29 @@
-
+const faker = require('faker')
 Feature('Home Page');
 
 Scenario('Open home page', (I) => {
     I.amOnPage('/')
     I.see('conduit')
-
+    pause()
 });
+
+Scenario('Publish article as new user @wip', (I) => {
+    I.amOnPage('/')
+    I.see('conduit')
+    I.click('Sign up')
+
+    I.fillField('Username', faker.name.firstName())
+    I.fillField("input[placeholder='Email']", faker.random.uuid() + faker.internet.email() )
+    I.fillField("//input[@placeholder='Password']", faker.commerce.color())
+
+    let signUpButton = locate('form').find(".btn")
+    I.click(signUpButton)
+
+    let newPost = locate('a').withAttr( {href: '/editor'})
+    I.waitForVisible(newPost)
+    I.click(newPost)
+
+    I.fillField('Article Title', faker.hacker.adjective())
+    I.click(signUpButton)
+
+})
